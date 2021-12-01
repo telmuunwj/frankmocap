55d5210ceea926ec9228a8dd01a430895ae26916
Telmuun文佳 <58360802+telmuunwj@users.noreply.github.com>
2021-12-01 15:59:28 +0800

    Update demo_api_inference.py

--- demo/demo_api_inference.py
+++ demo/demo_api_inference.py
@@ -27,18 +27,22 @@ from demo.demo_frankmocap import run_frank_mocap
 
 class Predict:
   def __init__(self):
-        self.args = DemoOptions().parse()
-        self.args.use_smplx = True
+      self.args = DemoOptions().parse()
+      self.args.use_smplx = True
+      self.args.out_dir = "./"
 
-        device = torch.device('cpu')
+      device = torch.device('cpu')
 
-        self.hand_bbox_detector =  HandBboxDetector('third_view', device)
+      self.hand_bbox_detector =  HandBboxDetector('third_view', device)
 
-        #Set Mocap regressor
-        self.body_mocap = BodyMocap(args.checkpoint_body_smplx, args.smpl_dir, device = device, use_smplx= True)
-        self.hand_mocap = HandMocap(args.checkpoint_hand, args.smpl_dir, device = device)
+      #Set Mocap regressor
+      self.body_mocap = BodyMocap(self.args.checkpoint_body_smplx, self.args.smpl_dir, device = device, use_smplx= True)
+      self.hand_mocap = HandMocap(self.args.checkpoint_hand, self.args.smpl_dir, device = device)
+  def predict(self, path):
+      self.args.input_path = path
+      
+      return {"result": run_frank_mocap(self.args, self.hand_bbox_detector, self.body_mocap, self.hand_mocap)}
 
-    def predict(self, path):
-        self.args.input_path = path
-        
-        return {"result": run_frank_mocap(self.args, self.hand_bbox_detector, self.body_mocap, self.hand_mocap)}
+if __name__ == "__main__":
+    predictor = Predict()
+    print(predictor.predict("/content/frankmocap/img/"))
